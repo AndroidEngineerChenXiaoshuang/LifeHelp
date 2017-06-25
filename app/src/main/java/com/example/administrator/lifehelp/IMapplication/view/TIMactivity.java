f0a1a9385983a028676afc7e08c09653e927759b
Jam <1143241513@qq.com>
2017-06-25 20:34:06 +0800

    Jam finish alter Animation BUG

--- app/src/main/java/com/example/administrator/lifehelp/IMapplication/view/TIMactivity.java
+++ app/src/main/java/com/example/administrator/lifehelp/IMapplication/view/TIMactivity.java
@@ -3,10 +3,6 @@ package com.example.administrator.lifehelp.IMapplication.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-
 import com.example.administrator.lifehelp.R;
 
 
