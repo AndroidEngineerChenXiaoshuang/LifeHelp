aa4db14a1d4e789745c047c19c36a114746989c1
Jam <1143241513@qq.com>
2017-06-25 20:25:47 +0800

    Commit alter BUG

---
+++ app/src/main/java/com/example/administrator/lifehelp/IMapplication/view/TIMactivity.java
@@ -0,0 +1,23 @@
+package com.example.administrator.lifehelp.IMapplication.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.administrator.lifehelp.R;
+
+
+/**
+ * 该类专门用于管理聊天的类
+ */
+
+public class TIMactivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timactivity);
+    }
+}
